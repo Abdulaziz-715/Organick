51251ff04e365412ec4f5445eb00e95dad316981
Abdulaziz-715 <abdulazizyunusov715@gmail.com>
2024-12-10 16:41:28 +0600

    da

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -24,15 +24,3 @@ const form = document.querySelector('.subscribe__form');
   }
   
   
-  window.onclick = function (event) {
-      if (!event.target.matches('.dropbtn')) {
-          var dropdowns = document.getElementsByClassName("dropdown-content");
-          var i;
-          for (i = 0; i < dropdowns.length; i++) {
-              var openDropdown = dropdowns[i];
-              if (openDropdown.classList.contains('show')) {
-                  openDropdown.classList.remove('show');
-              }
-          }
-      }
-  }
--- assets/js/team.js
+++ assets/js/team.js
@@ -23,16 +23,3 @@ const form = document.querySelector('.subscribe__form');
       document.getElementById("myDropdown").classList.toggle("show");
   }
   
-  
-  window.onclick = function (event) {
-      if (!event.target.matches('.dropbtn')) {
-          var dropdowns = document.getElementsByClassName("dropdown-content");
-          var i;
-          for (i = 0; i < dropdowns.length; i++) {
-              var openDropdown = dropdowns[i];
-              if (openDropdown.classList.contains('show')) {
-                  openDropdown.classList.remove('show');
-              }
-          }
-      }
-  }
